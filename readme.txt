50a2b3a044e96d05dfe084fc43835c9075df4875
gosunatxrea <gosunatxrea@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-17 01:55:49 +0000

    Adding first version of u2gg
    git-svn-id: https://plugins.svn.wordpress.org/u2gg/trunk@194666 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,20 @@
+=== Plugin Name ===
+Contributors: gosunatxrea
+Tags: wareki, gengou, date
+Requires at least: 2.8.5
+Tested up to: 2.9.1
+Stable tag: 0.1
+
+Plug-in to display the date WAREKI(GENGOU).
+
+
+== Description ==
+
+Plug-in to display the date WAREKI(GENGOU).
+
+
+== Installation ==
+
+1. Upload `u2gg.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Place `<?php u2gg(); ?>` in your templates
---
+++ u2gg.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+Plugin Name: u2gg
+Version: 0.1
+Plugin URI: http://shdb.info/
+Description: Plug-in to display the date WAREKI(GENGOU)
+Author: gosunatxrea@gmail.com
+Author URI: http://shdb.info/
+*/
+
+function u2gg() {
+	global $post;
+	
+	$utime = strtotime($post->post_date);
+
+	if ($utime >= strtotime("1989-01-08 00:00:00")) 
+	{ 
+		$gengo = '平成'; 
+		$year = date("Y", $utime) - 1988;
+	} 
+	elseif ($utime >= strtotime("1926-12-25 00:00:00"))
+	{ 
+		$gengo = '昭和';
+		$year = date("Y", $utime) - 1925;
+	}
+	elseif ($utime >= strtotime("1912-07-30 00:00:00"))
+	{ 
+		$gengo = '大正'; 
+		$year = date("Y", $utime) - 1911;
+	}
+	elseif ($utime >= strtotime("1868-09-08 00:00:00"))
+	{ 
+		$gengo = '明治'; 
+		$year = date("Y", $utime) - 1867;
+	}
+	else false;
+	
+	echo $gengo.$year.'年('.date("Y", $utime).') '.date("n", $utime).'月 '.date("j", $utime).'日';
+}
+
+?>
